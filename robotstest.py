88961e75d27d28d914e540cceb890b28c5e1743b
tdurkin18 <durkim@rpi.edu>
2019-12-04 19:43:17 -0500

    added simple testing for robots

--- robotstest.py
+++ robotstest.py
@@ -1,7 +1,11 @@
 from reppy.robots import Robots
 
+#grab robots url
 url = Robots.robots_url('https://science.rpi.edu/computer-science')
-robots = Robots.fetch(url)
 
-print(robots.allowed('https://science.rpi.edu/computer-science/', 'agent'))
-print(robots.allowed('https://science.rpi.edu/admin/', 'agent'))
+if 'http' in url:
+    #print(url)
+    robots = Robots.fetch(url)
+    #print(robots)
+    print(robots.allowed('https://science.rpi.edu/computer-science/', 'agent'))
+    print(robots.allowed('https://science.rpi.edu/admin/', 'agent'))
